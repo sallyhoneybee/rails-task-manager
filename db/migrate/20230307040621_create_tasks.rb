5a8d57119885cc9ce90f31362d517e0c081e2d96
Summer Chew <summerchewjx@gmail.com>
2023-03-07 12:09:00 +0800

    migrated

--- db/migrate/20230307040621_create_tasks.rb
+++ db/migrate/20230307040621_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.text :details
-      t.boolean :completed
+      t.string :title, null: false
+      t.text :details, null: false
+      t.boolean :completed, default: false, null: false
 
       t.timestamps
     end
